bb6757ca2f58542f966d58398fd83d6759edb398
Tushar Joshi <try.tusharjoshi@gmail.com>
2026-07-07 22:23:17 +0530

    Add: anagram-groups - submission-2

---
+++ Data Structures & Algorithms/anagram-groups/submission-2.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> m;
+        int n = strs.size();
+        for(int i=0;i<n;i++){
+            vector<int> freq(26,0);
+            string str = strs[i];
+            string key = "";
+            for(int j = 0;j<str.size();j++) {freq[str[j] - 'a']++;}
+            for(int j = 0;j<26;j++){
+
+                //frequency of the character in the string
+                int f = freq[j];
+                if(f){
+                char k = 'a' + j;
+                key+=k;
+                key+=char(f);
+                }
+            }
+            m[key].push_back(strs[i]);
+        }
+        vector<vector<string>> vec;
+        for(auto it : m){
+            vec.push_back(it.second);
+        }
+        return vec;
+    }
+};
