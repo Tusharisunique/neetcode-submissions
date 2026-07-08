33243b0718ab25c0a3e7eef3701b8fe0160aa66f
Tushar Joshi <try.tusharjoshi@gmail.com>
2026-07-08 14:41:49 +0530

    Add: string-encode-and-decode - submission-0

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-0.cpp
@@ -0,0 +1,20 @@
+class Solution {
+private:
+    unordered_map<string,vector<string>> m;
+public:
+
+    string encode(vector<string>& strs) {
+        string str;
+        int n = strs.size();
+        for(int i = 0;i<n;i++){
+            str+=strs[i];
+            str+='#';
+        }
+        m[str] = strs;
+        return str;
+    }
+
+    vector<string> decode(string s) {
+        return m[s];
+    }
+};
